5b8ccb328b23dfd93a2d37053ae13400f26b0e42
Rico Jasper <jasper@iscarsharing.com>
2014-04-07 21:09:02 +0200

    basic user support

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Tandem::Application.routes.draw do
 
   get "home/index"
 
+  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
