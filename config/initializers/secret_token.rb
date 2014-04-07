bcb36c687e48e2429eb5d6fb36b8c0ec8c6a7f25
Rico Jasper <jasper@iscarsharing.com>
2014-04-07 19:55:36 +0200

    initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tandem::Application.config.secret_key_base = '0bf39afaba892cce39c7c37a7941c782f30b12b0b0cb82610406ff18a9c04d0b0e061d90a924d0cec43dd82d33ce2e4643cda75021960644e8d7c67e03e69c92'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tandem::Application.config.session_store :cookie_store, key: '_tandem_session'
